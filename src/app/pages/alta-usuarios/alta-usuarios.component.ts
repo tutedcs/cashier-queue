ce1e401b04dced86eea21c2798a165833feee621
lostdou <facundobott@gmail.com>
2025-03-24 02:32:37 -0300

    menu "alta usuarios" terminada
    Co-authored-by: Matias Dante Sarco <matiasdante@users.noreply.github.com>
    Co-authored-by: Luciano Cayssials <Shackmerferu@users.noreply.github.com>

--- src/app/pages/alta-usuarios/alta-usuarios.component.ts
+++ src/app/pages/alta-usuarios/alta-usuarios.component.ts
@@ -6,6 +6,7 @@ import { NavbarComponent } from '../../navbar/navbar.component';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { UsuariosService } from '../../services/usuarios.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-alta-usuarios',
@@ -17,7 +18,8 @@ import { UsuariosService } from '../../services/usuarios.service';
 export class AltaUsuariosComponent {
   form: FormGroup;
   public usuarios: any[] = [];
-
+  public showPassword: boolean = false; // Variable para controlar la visibilidad de la contraseña
+  
   constructor(private router: Router, 
     private fb: FormBuilder, 
     private usuarioSv: UsuariosService,
@@ -32,6 +34,10 @@ export class AltaUsuariosComponent {
   }
 
   ngOnInit() {
+    this.getUsuarios();
+  }
+
+  getUsuarios() {
     this.usuarioSv.getUsuarios().subscribe((data: any) => {
       this.usuarios = data;
       console.log(this.usuarios);
@@ -39,7 +45,93 @@ export class AltaUsuariosComponent {
   }
 
   onSubmit() {
+    if (this.form.valid){
+      this.loginSv.register(this.form.value).subscribe((data: any) => {
+        if (data.code==='USER_REGISTERED'){
+          Swal.fire({
+            icon: 'success',
+            title: 'Usuario registrado',
+            text: 'El usuario se ha registrado correctamente',
+            timer: 2000,
+            showConfirmButton: false,
+            timerProgressBar: true
+          }).then((result) => {
+            this.getUsuarios();
+          });
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Error al registrar',
+            text: 'Ha ocurrido un error al registrar el usuario',
+            timer: 4000,
+            showConfirmButton: false,
+            timerProgressBar: true
+          });
+        }
+      });
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Campos requeridos',
+        text: 'Por favor, rellene todos los campos',
+        timer: 3000,
+        showConfirmButton: false,
+        timerProgressBar: true,
+        toast: true,
+        position: 'top-end'
+      });
+    } 
+  }
 
+  desactivarUsuario(idUsuario: number) {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger m-2"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: '¿Estás seguro? Vas a dar de baja a un usuario',
+      text: "No podra usar más el sistema hasta que lo vuelvan a dar de alta",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, desactivar',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.usuarioSv.desactivarUsuario(idUsuario).subscribe((data: any) => {
+          console.log(data);
+          if (data.code==='200'){
+            Swal.fire({
+              icon: 'success',
+              title: 'Usuario desactivado',
+              text: 'El usuario se ha desactivado correctamente',
+              timer: 2000,
+              showConfirmButton: false,
+              timerProgressBar: true
+            }).then((result) => {
+              this.getUsuarios();
+            });
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: 'Error al desactivar',
+              text: 'Ha ocurrido un error al desactivar el usuario',
+              timer: 4000,
+              showConfirmButton: false,
+              timerProgressBar: true
+            });
+          }
+        });
+      }
+    });
+
+    console.log(idUsuario);
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
   }
 
 }
--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -17,4 +17,20 @@ export class UsuariosService {
     getUsuarios(): Observable<any> {
         return this.http.get(this.API_URL + 'Listar');
     }
+
+    searchUsuario(nombre: string, apellido: string): Observable<any> {
+        const params = {
+            nombre: nombre,
+            apellido: apellido
+        }
+        return this.http.get(this.API_URL + 'Buscar', { params });
+    }
+
+    desactivarUsuario(idUsuario: number): Observable<any> {
+        return this.http.put(this.API_URL + 'Desactivar/' + idUsuario, {});
+    }
+
+    activarUsuario(idUsuario: number): Observable<any> {
+        return this.http.put(this.API_URL + 'Activar/' + idUsuario, {});
+    }
 }
